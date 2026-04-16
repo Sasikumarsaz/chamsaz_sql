afe2aa2d322a0e8ae5397abc9f3c710afb353c03
Mahasundari Mathivanan <mahasundarimathivanan10@gmail.com>
2026-04-17 03:04:14 +0530

    Update Distinct and Count.sql

--- Distinct and Count.sql
+++ Distinct and Count.sql
@@ -1,12 +1,12 @@
 --This is to find the list of duplicate values from column and displays as unique (removes dupicate entry)
  
-SELECT DISTINCT(rating) FROM film;
+SELECT DISTINCT(column1) FROM table;
 
 --Count helps to count the number of entries in the column, and this always says the number of rows.
 
-SELECT COUNT (rating) FROM film
+SELECT COUNT(column1) FROM table;
 
 
---We can also merge with distinct, which will remove the duplicate entry
+--We can also merge count with distinct, which will remove the duplicate entry
 
-SELECT COUNT (DISTINCT rating) FROM film
+SELECT COUNT (DISTINCT column1) FROM table;
