33fc0136329d4b3d1a3cf922c05f1bbeb48a10a4
Mahasundari Mathivanan <mahasundarimathivanan10@gmail.com>
2026-04-17 02:48:51 +0530

    Update List of tables.sql

--- List of tables.sql
+++ List of tables.sql
@@ -14,3 +14,11 @@ WHERE table_schema
 NOT IN ('information_schema', 'pg_catalog')   
 AND table_type = 'BASE TABLE'
 ORDER BY table_schema, table_name;
+
+--list of column from default schema and selective table
+  
+SELECT column_name
+FROM information_schema.columns
+WHERE table_schema = 'public' 
+AND table_name = 'your table name'
+ORDER BY ordinal_position;
